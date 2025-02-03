7328f42dda716bde093efb08e3c41e8a73fd79b5
buchhavee <mach0015@stud.kea.dk>
2025-02-03 14:55:21 +0100

    add product list

--- produktliste.js
+++ produktliste.js
@@ -1,7 +1,7 @@
-const category = "Apparel";
+const myCategory = new URLSearchParams(window.location.search).get("category");
 const listContainer = document.querySelector("#productContainer");
 
-fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
